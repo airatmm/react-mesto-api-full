9a2ca33246e40aeb9201e671b7fbff162d00d0b7
Ayrat Mukhametshin <airatmm@gmail.com>
2022-06-02 20:04:59 +0300

    sameSite none signout

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -93,7 +93,7 @@ const login = async (req, res, next) => {
     res.cookie('jwt', token, {
       maxAge: 3600000 * 24 * 7,
       httpOnly: true,
-      sameSite: 'Strict',
+      sameSite: 'none',
       secure: true,
     });
     res.status(200).send({ token });
